d8b78643b7d757d358493ab4abfc2521e8283fdb
Riki420 <girottiriccardo1998@gmail.com>
2021-07-06 18:03:49 +0200

    correzione del secondo while e prova di .indexOf

--- js/script.js
+++ js/script.js
@@ -2,19 +2,18 @@
 // 2. inseriscilo in un array con altri cognomi: ‘Bianchi’, ‘Neri’, ‘Rossi’, ‘Verdi’, ‘Gialli’
 // 3. stampa la lista ordinata alfabeticamente sia in console che in pagina
 // 4. scrivi anche la posizione "umana" (partendo da 1) della lista in cui il nuovo utente si trova
+ 
 
 
+// INITIAL VARIABLES
 var lastName = ['Bianchi', 'Neri', 'Rossi', 'Verdi', 'Gialli'];
 var userName
 
-do{
-    userName =
+userName =
     prompt('inserisci il tuo cognome').trim();
     lastName.push(userName.charAt(0).toUpperCase() + userName.slice(1));
-} while(!userName);
 
-
-// Check
+// VALIDATION
 
 var isName = false;
 
@@ -27,7 +26,6 @@ while(i < lastName.length && !isName){
 }
 
 
-
 if(isName){
     lastName.sort();
 }
@@ -35,17 +33,18 @@ if(isName){
 var listName = '';
 
 var i = 0; 
-while(i <= lastName.length){
+while(i < lastName.length){
     listName += '<li>' + lastName[i] + '</li>';
     i++;
 }
 
 
-
+// PRINT LIST AND MESSAGE
 document.getElementById('list').innerHTML = listName.charAt(0).toUpperCase() + listName.slice(1);
 
 lastName = listName;
 
-var message = 'il tuo cognome è nella posizione: ' + listName[i];
+var message = 'il tuo cognome è nella posizione: ';
 
-document.getElementById('display').innerHTML = message;
+
+document.getElementById('display').innerHTML = message + lastName.indexOf(userName);
